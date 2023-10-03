ee76ce0c6ef0419c632e3542ab4f2d5a055c963d
mnflaviosebastiao <112144273+mnflaviosebastiao@users.noreply.github.com>
2023-10-03 12:08:14 +0000

    Update configGestor.js

--- fl_2/assets/configGestor.js
+++ fl_2/assets/configGestor.js
@@ -1,9 +1,9 @@
 const CONFIG_GESTOR = {
     checkoutLinks: {
-        opcao1: 'https://checkout.payt.com.br/5b8e9841e87fb8fc590434f5d933c92c?split=12&src=Flavio_PV_antiga',
-        opcao2: 'https://checkout.payt.com.br/ee26fc66b1369c7625333bedafbfcaf6?split=12&src=flavio_PV_antiga',
-        opcao3: 'https://checkout.payt.com.br/d542599794c1cf067d90638b5d3911f3?split=12&src=flavio_PV_antiga',
-        opcao4: 'https://checkout.payt.com.br/697e382cfd25b07a3e62275d3ee132b3?split=12&src=flavio_PV_antiga'
+        opcao1: 'https://checkout.payt.com.br/36920c7628b5bc6824378f3411bbfa34?src=Flavio_PV_antiga',
+        opcao2: 'https://checkout.payt.com.br/df01683a169bdcc3853ab5bd852a9832?src=flavio_PV_antiga',
+        opcao3: 'https://checkout.payt.com.br/90d89eccfe88456d58a2872a9350f6a9?src=flavio_PV_antiga',
+        opcao4: 'https://checkout.payt.com.br/94546c6c568fd629ae0799b223921ca9?src=flavio_PV_antiga'
     }
 };
 
